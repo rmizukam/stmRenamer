ae5f62a2dd7260dffc8f5700c331b809514ba475
Ryan Mizukami <rmizukami741@gmail.com>
2024-07-28 23:29:00 -0700

    list of months for text replacement is done using txt filesnow

--- FRenamer.h
+++ FRenamer.h
@@ -12,17 +12,26 @@ class FRenamer {
         std::vector<std::filesystem::path> oldNames;
         std::string nameToUse;
 
-        std::vector<std::string> readAlias(std::string txtPath);
+        std::string convertNum2Str(int num);
+        std::filesystem::path getRepoRoot();
+        std::vector<std::string> readTxtFile(std::string txtPath);
+        std::vector<std::string> readTxtFile(std::filesystem::path txtPath);
         void setOldNames(std::vector<std::filesystem::path> old);
         void initiateNewNames();
-        std::tuple<bool, std::string> isNameInFileName(std::vector<std::string> aliasTxt, std::string filePath);
-        std::tuple<bool, std::string> isNameInFileName(std::vector<std::string> aliasTxt, std::filesystem::path filePath);
-        std::string detectNameFormat(std::filesystem::path filePath);
+        std::tuple<bool, std::string> isNameInFileName(std::vector<std::string> aliasTxt, std::string filePath, std::string userInput);
+        std::tuple<bool, std::string> isNameInFileName(std::vector<std::string> aliasTxt, std::filesystem::path filePath, std::string userInput);
+        std::string reformatName(std::vector<std::string> aliasTxt, std::filesystem::path filePath, std::string name);
         std::vector<std::string> extractNums(std::string);
+        std::string getIndex(std::vector<std::string> v, std::string K);
+        bool startsWith(const std::string& fullString, const std::string& beginning);
+        std::string join(std::vector<std::string> stringVec, std::string delimeter);
+        std::string join(std::vector<std::string> stringVec);
+        std::vector<std::string> removeNumsInName(std::vector<std::string> numsFromName, int amtNumsInName);
 
         int extractAmtNumInName(std::string);
-        std::vector<std::string> extractDateString(std::string name, std::string str);
+        std::string extractExtraInfo(std::string inStr);
         std::vector<std::string> splitNumStr(std::string numStr);
+        std::string convertMonthString2MonthIdx(std::string fName);
         
         FRenamer();
         FRenamer(std::vector<std::filesystem::path> old);
--- stmRenamer.cpp
+++ stmRenamer.cpp
@@ -8,17 +8,18 @@ int main(){
     PrintUtils printer;
     DirParser dir;
     dir.getUserInput();
-    std::vector<std::string> aliasTxt {dir.renamerObj.readAlias(dir.aliasPath.string())};
+    std::vector<std::string> aliasTxt {dir.renamerObj.readTxtFile(dir.aliasPath.string())};
     
     for (auto pathName : dir.directoryContents){
-        std::tuple<bool, std::string> nameInFile {dir.renamerObj.isNameInFileName(aliasTxt, pathName)};
-        std::string nameFormat { dir.renamerObj.detectNameFormat(pathName) };
-        // std::vector<std::string> dateStr {dir.renamerObj.extractDateString(std::get<std::string>(nameInFile), pathName.filename().string())};
-        
+        std::tuple<bool, std::string> nameInFile {dir.renamerObj.isNameInFileName(aliasTxt, pathName, dir.userInput)};
+        std::string nameFormat { dir.renamerObj.reformatName(pathName, dir.userInput) };
+        printer.print(nameFormat);
+        std::cout << std::endl;
     }
     
     // std::string oldname = "D:\\WD Files\\WD Software\\Fraps\\_Movie\\2024-07-18 06-17-29.mkv";
     // std::string newname = "D:\\WD Files\\WD Software\\Fraps\\_Movie\\banana.mkv";
 
     // std::rename(oldname.c_str(), newname.c_str());
+
 }
